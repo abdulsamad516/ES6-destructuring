bde038ad1c8e1f55fa45911747f86bf2fa50516f
Abdul Samad <75996134+abdulsamad516@users.noreply.github.com>
2021-03-23 17:16:03 +0500

    Add files via upload

---
+++ assignment no 1/app.js
@@ -0,0 +1,38 @@
+//DESTURCTURING 
+
+// const myArray = ['samad','18','developer']
+// const [name,,profession] = myArray
+// console.log(name,profession)
+
+// const myArray = [10,20,30,40,50]
+// const [a,b, ...rest] = myArray 
+// console.log(a,b,rest)
+
+
+//DESTRUCTURING OF NESTED ARRAY
+
+// const myArray = [10, 20, 30, 40, [11,22,33,44]];
+// const [a,c, ...rest] = myArray;
+// console.log(a,  c, rest)
+
+
+//OBJECT LITERAL
+
+// let name = 'samad'
+// let school = 'KGS'
+
+// const profile = {
+//     name,
+//     school
+// }
+
+// console.log(...Object.keys(profile))
+// console.log(...Object.values(profile))
+// console.log(...Object.entries(profile))
+
+//     const keys = Object.keys(profile)
+
+//     for(let index = 0; index < keys.length; index++){
+//         console.log(profile[keys[index]]);
+//     }
+
